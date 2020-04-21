0ed33c85b9d58b06968205ff70d1115e442b17c6
Pierre Hérissé <pierre.herisse@gmail.com>
2020-04-21 12:08:56 +0200

    Add level of data to get sample or live datas

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,17 +2,18 @@ import './prelude'
 import './postlude'
 import ExpressServer from './core/ExpressServer'
 import Scraper from './core/Scraper'
+;(async () => {
+	await ExpressServer.run()
 
-(async () => {
-  await ExpressServer.run()
-
-  try {
-    // 1st param : the type of the data to scrape ("movies" or "series")
-    // 2nd param : the size of data requested ("sample" for 100, "live" for 5000)
-    await Scraper.scrape('movies', 'sample')
-    await Scraper.scrape('series', 'sample')
-  } catch(e) {
-    console.log('Error while scrapping')
-    console.log(e)
-  }
+	try {
+		// 1st param : the type of the data to scrape ("movies" or "series")
+		// 2nd param : the size of data requested ("sample" for 100, "live" for 5000)
+		await Scraper.scrape('movies', 'sample')
+		await Scraper.scrape('series', 'sample')
+    return
+	} catch (e) {
+		console.log('Error while scrapping')
+		console.log(e)
+    return e
+	}
 })()
