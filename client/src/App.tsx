44a04a25f2361bdd6644dee4c0c88208dd96e996
xDeaa <andrea_christina@hotmail.fr>
2020-04-21 16:39:22 +0200

    [EVOL] Beggin page profile

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import 'rsuite/dist/styles/rsuite-dark.css';
 import { Login, Home } from './pages'
 import User from './core/user';
 import Register from './pages/Register';
+import Profile from './pages/Profiles';
 
 const user = localStorage.getItem('user')
 
@@ -32,9 +33,32 @@ const reducer = (prevState: UserState, action: UserAction): UserState => {
 function App(): JSX.Element {
   return (
     <User.Provider initialState={userInitialState} reducer={reducer}>
+<<<<<<< HEAD
       <Route exact path="/login" component={Login} />
       <Route exact path="/home" component={Home} />
       <Route exact path="/register" component={Register} />
+=======
+      <Route
+        exact
+        path="/login"
+        component={Login}
+      />
+      <Route
+        exact
+        path="/register"
+        component={Register}
+        />
+        <Route
+        exact
+        path="/profile"
+        component={Profile}
+        />
+      <Route
+        exact
+        path="/home"
+        component={Home}
+      />
+>>>>>>> [EVOL] Beggin page profile
     </User.Provider>
   )
 }
