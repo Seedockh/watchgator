1d0bb4c66ceefd479c77fa233d632b7d48b5e76b
BLECUONA <benjaminlecuona@gmail.com>
2020-04-27 16:33:00 +0200

    api[DEV](user) added elete user rest endpoint

--- server/src/routes/secured/user.ts
+++ server/src/routes/secured/user.ts
@@ -5,6 +5,7 @@ const router = Router()
 
 router.get('/get/:uuid', UserAuthController.getUser)
 router.put('/update-avatar', UserAuthController.updateAvatar)
-router.delete('/remove-avatar/:fileKey', UserAuthController.deleteAvatar)
+router.delete('/delete-avatar/:fileKey', UserAuthController.deleteAvatar)
+router.delete('/delete/:uuid', UserAuthController.deleteUser)
 
 export default router
