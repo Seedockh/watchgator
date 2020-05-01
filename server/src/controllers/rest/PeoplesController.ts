11ba64f0f49f28de3f141f3bd2daa7244351cec7
Seedockh <pierre.herisse@gmail.com>
2020-05-02 01:51:29 +0200

    ignore unreachable keys

--- server/src/controllers/rest/PeoplesController.ts
+++ server/src/controllers/rest/PeoplesController.ts
@@ -9,7 +9,9 @@ const level: string = process.env.NODE_ENV === 'production' ? 'live' : 'sample'
 
 class PeoplesController {
 	static getAll(req: Request, res: Response) {
+		// @ts-ignore: unreachable key
 		const total = IMDBDatasetService[`${level}Peoples`].data.length
+		// @ts-ignore: unreachable key
 		const results = _.chunk(IMDBDatasetService[`${level}Peoples`].data, 20)
 
 		res.json({ total: total, pages: results.length, results: results })
@@ -19,12 +21,14 @@ class PeoplesController {
 		const page = parseInt(req.params.page)
 		const start = 20 * page
 		const end = start + 20
+		// @ts-ignore: unreachable key
 		const result = _.slice(IMDBDatasetService[`${level}Peoples`].data, start, end)
 
 		res.json({ total: result.length, page: page+1, results: result })
 	}
 
 	static getById(req: Request, res: Response) {
+		// @ts-ignore: unreachable key
 		res.json(_.find(IMDBDatasetService[`${level}Peoples`].data, { id: req.params.id }))
 	}
 
@@ -40,6 +44,7 @@ class PeoplesController {
 		)
 
 		let results = _.filter(
+			// @ts-ignore: unreachable key
 			IMDBDatasetService[`${level}Peoples`].data,
 			people => {
 				for (const key in filters) {
