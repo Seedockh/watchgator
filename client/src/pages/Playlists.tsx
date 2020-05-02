a672bf74013b496486c106fe75ffeff8dd36c8cd
xDeaa <andrea_christina@hotmail.fr>
2020-05-02 14:52:30 +0200

    add redirect when click on image

--- client/src/pages/Playlists.tsx
+++ client/src/pages/Playlists.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { Container, Content, Panel, Divider, Badge } from 'rsuite'
 import Coverflow from 'react-coverflow'
+import { useHistory } from 'react-router-dom'
 
 import User from '../core/user'
 import { Sidebar } from '../widget/sidebar/Sidebar'
@@ -8,6 +9,7 @@ import { playlists } from '../data/playlists'
 
 const Playlists = () => {
     const [{ user }, dispatch] = User.GlobalState()
+    const history = useHistory();
 
     useEffect(() => {
         console.log(`user: ${JSON.stringify(user)}`)
@@ -29,26 +31,13 @@ const Playlists = () => {
                                 <h3>{playlist.name} <Badge content={playlist.movies.length} style={{ backgroundColor: "green", fontSize: 15 }} /></h3>
                                 <Divider />
                                 <Coverflow
-                                    width="100%"
-                                    height="100%"
-                                    displayQuantityOfSide={5}
+                                    displayQuantityOfSide={3}
                                     navigation={false}
                                     enableHeading={false}
                                     enableScroll={false}
-                                    active={0}
-
                                 >
-                                    <div
-                                        // onClick={() => fn()}
-                                        // onKeyDown={() => fn()}
-                                        role="menuitem"
-                                        tabIndex={0}
-                                    >
-
-                                    </div>
                                     {playlist.movies.map(movie => (
-                                        <img src={movie.imageUrl} alt='title or description' />
-
+                                        <img src={movie.imageUrl} alt='title or description' onClick={() => history.push(`/movie/${movie.id}`)} />
                                     ))}
                                 </Coverflow>
                             </Content>
