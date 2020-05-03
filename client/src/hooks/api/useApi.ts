7c156ffccccf922ef9f34b4c5a74abf728925fc3
xDeaa <andrea_christina@hotmail.fr>
2020-05-03 19:47:22 +0200

    try link user

--- client/src/hooks/api/useApi.ts
+++ client/src/hooks/api/useApi.ts
@@ -33,6 +33,7 @@ export function useApi<T>(route: string, method = 'GET', body?: string): ApiHook
 }
 
 export const useAllCategories = (): ApiHook<string[]> => useApi<string[]>('/genres/all/');
+// export const useConnectUser = (values: object): ApiHook<string[]> => return () useApi<string[]>('/auth/signin', "POST", JSON.stringify(values));
 
 export const useSearchActors = (): ApiHookSearch<BaseResponse<Actor[][]>> => {
     const [query, setQuery] = useState<string>('')
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,12 +1,21 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import { Container, Content, ControlLabel, FlexboxGrid, Form, Panel, FormGroup, FormControl, Button } from 'rsuite'
-import { useInput } from '../hooks/useInput'
+
 import User from '../core/user'
+import { useApi, useConnectUser } from '../hooks/api/useApi'
+import { ApiHook } from '../models/ApiHook';
+
 
 const Login = () => {
-  const { bind: emailBind } = useInput('')
-  const { bind: passwordBind } = useInput('')
+  // const { bind: emailBind } = useInput('')
+  // const { bind: passwordBind } = useInput('')
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const values = {email, password}
+
+  //const { isLoading: categoriesLoading, data: categories } = useConnectUser(values);
+
   const [{ user }] = User.GlobalState()
   const history = useHistory()
 
@@ -32,6 +41,15 @@ const Login = () => {
     history.push('/register')
   }
 
+  const handleSubmit = () => {
+    
+    //const response = useApi<string[]>('/auth/signin', "POST", JSON.stringify(values));
+    const response = useConnectUser(values);
+    console.log(response);
+    
+
+  }
+
   return (
     <Container style={{ marginTop: 200 }}>
       <Content>
@@ -41,15 +59,15 @@ const Login = () => {
               <Form fluid>
                 <FormGroup>
                   <ControlLabel>Email address</ControlLabel>
-                  <FormControl name="name" {...emailBind} />
+                  <FormControl name="name" onChange={value => setEmail(value)} />
                 </FormGroup>
                 <FormGroup>
                   <ControlLabel>Password</ControlLabel>
-                  <FormControl name="password" type="password" {...passwordBind} />
+                  <FormControl name="password" type="password" onChange={value => setPassword(value)} />
                   <Button appearance="link">Forgot password?</Button>
                 </FormGroup>
                 <FormGroup className='flex flex-column flex-align-center'>
-                  <Button appearance="primary" style={{ width: 150 }} onClick={() => history.push('/')}>Login</Button>
+                  <Button appearance="primary" style={{ width: 150 }} onClick={handleSubmit}>Login</Button>
                   <FormGroup className='flex' style={{ alignItems: "baseline", marginTop: 10 }}>
                     <ControlLabel>Don't have account ?</ControlLabel>
                     <Button onClick={redirectRegister} appearance="link"> Sign up</Button>
