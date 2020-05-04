c23afcbd12b09b7d0f9124943659826692d6d0c1
Seedockh <pierre.herisse@gmail.com>
2020-05-05 01:40:58 +0200

    add results and request time below Movies title

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -22,10 +22,31 @@ export const Home = () => {
           <Searchbar onChange={setSearch} />
         </Header>
         <Content>
+<<<<<<< HEAD
           {search && search.trim().length > 0
             ? <Search query={search} />
             : <HomeMovies filters={filters} />
           }
+=======
+          <Panel className="mb-6">
+            <h1 className="ml-4">{displayTabTitles()}</h1>
+            <span style={{ color: 'gray', fontSize: 15, marginLeft: '1.2em' }}>{moviesFetch.data ? `${moviesFetch.data.totalMovies} results, ${moviesFetch.data.time}ms` : 'searching...' }</span>
+            <Grid fluid >
+              {moviesFetch.isLoading
+                ? <Row style={loaderStyle}>
+                  <Loader size="lg" />
+                </Row>
+                : <Row>
+                  {(tab === 'movies' ? movies : series).map((item) => (
+                    <Col key={item.id} xs={24} sm={12} md={6} lg={4} >
+                      <MovieCard movie={item} />
+                    </Col>
+                  ))}
+                </Row>
+              }
+            </Grid>
+          </Panel>
+>>>>>>> add results and request time below Movies title
         </Content>
       </Container>
     </Container>
