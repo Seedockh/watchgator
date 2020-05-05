21bdd3934342ca8b487ba7bd59b4c9c827866fa7
BLECUONA <benjaminlecuona@gmail.com>
2020-05-05 11:50:20 +0200

    api[TEST] drop db between real tests

--- server/tests/controllers/graphQl/resolversRealTest.ts
+++ server/tests/controllers/graphQl/resolversRealTest.ts
@@ -3,75 +3,43 @@ import request from 'superagent'
 import User from '../../../src/database/models/User'
 
 const resolversRealTest = (server: supertest.SuperTest<supertest.Test>) =>
-	describe('GraphQL - Auth routes', () => {
-		const userWithCorrectData: User = new User()
-		userWithCorrectData.nickname = 'Bob'
-		userWithCorrectData.password = 'bob1'
-		userWithCorrectData.email = 'bob1@gmail.com'
-		const { nickname, email, password } = userWithCorrectData
-		describe('GraphQL - Sign Up routes', () => {
-			it('Sign Up with correct data should return 200 and body should contains a User', async done => {
-				const query = `mutation {
-					signUp(${nickname}, ${email}, ${password})
-                    {uuid, nickname, email}
-                }`
+describe('GraphQL - Auth routes', () => {
 
-				const res: request.Response = await server
-					.post('/graphql')
-					.set('Accept', 'application/json')
-					.send({ query })
-				expect(res.status).toBe(200)
-				expect(res.body.data).toBeDefined()
-				expect(res.body.errors).toBeUndefined()
-				done()
-			})
-			it('Sign Up with an already used nickname should return 200 AND body should contains errors', async done => {
-				const query = `mutation {
-					signUp(${nickname}, ${email}, ${password})
-                    {uuid, nickname, email}
-                }`
-				const res: request.Response = await server
-					.post('/graphql')
-					.set('Accept', 'application/json')
-					.send({ query })
-				expect(res.status).toBe(200)
-				expect(res.body.data).toBeDefined()
-				expect(res.body.errors).toBeDefined()
-				expect(res.body.errors[0].extensions.code).toBe('UNAUTHENTICATED')
-				done()
-			})
-		})
-		// describe('GraphQL - Sign In routes', () => {
-		// 	it('Sign In with correct data should return an error', async done => {
-		// 		const query = `mutation {
-		//             signIn(nickname: "bobi22", email: "bob@gmail.com", password: "boob1")
-		//             {uuid, nickname, email}
-		//         }`
-		// 		const res: request.Response = await server
-		// 			.post('/graphql')
-		// 			.set('Accept', 'application/json')
-		// 			.send({ query })
-		// 		expect(res.status).toBe(200)
-		// 		expect(res.body.data).toBeDefined()
-		// 		expect(res.body.errors).toBeUndefined()
-		// 		done()
-		// 	})
-		// 	it('Sign In with inexistant user', async done => {
-		// 		const query = `mutation {
-		//             signIn(nickname: "inexistantNickName", email: "bob@gmail.com", password: "boob1")
-		//             {uuid, nickname, email}
-		//         }`
-		// 		const res: request.Response = await server
-		// 			.post('/graphql')
-		// 			.set('Accept', 'application/json')
-		// 			.send({ query })
-		// 		expect(res.status).toBe(200)
-		// 		expect(res.body.data).toBeNull()
-		// 		expect(res.body.errors).toBeDefined()
-		// 		expect(res.body.errors[0].extensions.code).toBe('INTERNAL_SERVER_ERROR')
-		// 		done()
-		// 	})
-		// })
-	})
+	describe('GraphQL - Sign Up routes', () => {
+		it('Sign Up with correct data should return 200 and body should contains a User', async done => {
+			const query = `mutation {
+				signUp(nickname: "bobi22", email: "bob@gmail.com", password: "boob1")
+				{uuid, nickname, email}
+			}`;
+			const res: request.Response = await server
+				.post('/graphql')
+				.set('Accept', 'application/json')
+				.send({ query });
+			console.log("res.body.errors")
+			console.log(res.body.errors)
+			expect(res.status).toBe(200);
+			expect(res.body.data).toBeDefined();
+			expect(res.body.errors).toBeUndefined();
+			done();
+		});
+		it('Sign Up with an already used nickname should return 200 AND body should contains errors', async done => {
+			const query = `mutation {
+				signUp(nickname: "bobi22", email: "bob@gmail.com", password: "boob1")
+				{uuid, nickname, email}
+			}`;
+			const res: request.Response = await server
+				.post('/graphql')
+				.set('Accept', 'application/json')
+				.send({ query });
+			expect(res.status).toBe(200);
+			expect(res.body.data).toBeDefined();
+			expect(res.body.errors).toBeDefined();
+			expect(res.body.errors[0].extensions.code).toBe(
+				'UNAUTHENTICATED',
+			);
+			done();
+		});
+	});
+});
 
 export default resolversRealTest
